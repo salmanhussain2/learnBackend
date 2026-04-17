28455c821fd5c4d22bc395321a5f9111382a631d
salmanhussain2 <salmanhussain9057@gmail.com>
2026-04-17 22:38:05 +0530

    crud apis integrated

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import {createPost} from '../controllers/post.controller.js';
+import {createPost, getPosts, updatePost, deletePost} from '../controllers/post.controller.js';
 //
 router.route('/createPost').post(createPost);
+router.route('/getPosts').get(getPosts);
+router.route('/updatePost/:id').patch(updatePost);
+router.route('/deletePost/:id').delete(deletePost);
 
 export default router;
